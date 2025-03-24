37f2862caa333af3b6b61c96dee92c0464938815
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-03-24 11:24:35 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 plr = sprites.create(assets.image("""p"""), SpriteKind.player)
-
-
 user_words = []
 
 prompts = [
@@ -14,15 +12,21 @@ for prompt in prompts:
     word = game.ask_for_string(prompt)
     user_words.append(word)
 
+
+
 story = "Last month, my family and I went on a trip.\n"
 story += "We went to " + user_words[0] + " which was " + user_words[1] + "!\n"
 story += "It took us " + user_words[2] + " to get there.\n"
 story += "We even got to see " + user_words[3] + " there. We took a bunch of photos of it."
-
 game.splash(story)
 
 
-star_list = []
+
+
+star_list : List[Sprite] = []
+
+
+
 
 
 for i in range(10):
@@ -32,6 +36,7 @@ for i in range(10):
     pause(100)
 
 
-for star in star_list:
-    star.set_flag(SpriteFlag.INVISIBLE, True)
+
+for s in star_list:
+    s.set_flag(SpriteFlag.INVISIBLE, True)
     pause(100)
--- main.ts
+++ main.ts
@@ -13,14 +13,14 @@ story += "We went to " + user_words[0] + " which was " + user_words[1] + "!\n"
 story += "It took us " + user_words[2] + " to get there.\n"
 story += "We even got to see " + user_words[3] + " there. We took a bunch of photos of it."
 game.splash(story)
-let star_list = []
+let star_list : Sprite[] = []
 for (let i = 0; i < 10; i++) {
     star = sprites.create(assets.image`st`, SpriteKind.Food)
     star.setPosition(randint(0, 160), randint(0, 120))
     star_list.push(star)
     pause(100)
 }
-for (star of star_list) {
-    star.setFlag(SpriteFlag.Invisible, true)
+for (let s of star_list) {
+    s.setFlag(SpriteFlag.Invisible, true)
     pause(100)
 }
